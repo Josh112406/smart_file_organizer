4eb81c16d10c84405a6b020b7f36326e44be583b
Josh112406 <quibaljosh@gmail.com>
2026-03-03 23:11:25 +0800

    add list_files function

--- organizer.py
+++ organizer.py
@@ -1,18 +1,25 @@
 import shutil
 import os
+import exceptions as e
 import utils
-
 class FileOrganizer:
-    def __init__(self, folder_path):
+    def __init__(self, folder_path: str = "."):
         self.folder_path = folder_path
-        
-    def list_files(self):
+
+    def list_files(self) -> list:
         #list all files from the given folder_path
-        pass
-    
-    def check_type(self):
+        try:
+            list_directory = os.listdir(self.folder_path)
+            file_list = [file for file in list_directory if os.path.isfile(file)]
+            utils.create_folder(self.folder_path)
+            return file_list
+        except (FileNotFoundError, OSError):
+            raise e.FolderNotFound()
+        
+    def check_type(self, file_list: list):
         #check file type
         pass
     
     def organize(self):
         pass
+file = FileOrganizer().list_files()
