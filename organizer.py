8f79c7a8cd8e0fb04f45a84de12c954db8a6b3c4
Josh112406 <quibaljosh@gmail.com>
2026-03-03 22:27:51 +0800

    create FileOrganizer class and functions

---
+++ organizer.py
@@ -0,0 +1,18 @@
+import shutil
+import os
+import utils
+
+class FileOrganizer:
+    def __init__(self, folder_path):
+        self.folder_path = folder_path
+        
+    def list_files(self):
+        #list all files from the given folder_path
+        pass
+    
+    def check_type(self):
+        #check file type
+        pass
+    
+    def organize(self):
+        pass
