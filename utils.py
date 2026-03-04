6440090e05464cd69ad843749416bea8e107b096
Josh112406 <quibaljosh@gmail.com>
2026-03-05 01:11:56 +0800

    add 'Duplicates'

--- utils.py
+++ utils.py
@@ -1,11 +1,14 @@
 import os
 import shutil
+import hashlib
 
 def create_folder(folder_path) -> None:
-    folder_names = ["Images", "Videos", "Audios", "Documents", "Code", "Others"]
+    folder_names = ["Images", "Videos", "Audios", "Documents", "Code", "Others", "Duplicates"]
     for folder in folder_names:
         if not os.path.exists(f"{folder_path}/{folder}"):
             os.mkdir(f"{folder_path}/{folder}")
         
 def move_file(file_source: str, file_dest: str):
     shutil.move(file_source, file_dest)
+    
+    
