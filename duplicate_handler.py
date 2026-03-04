879f587d4d980f8c6d15fe3544e6f8ed2ba6bad4
Josh112406 <quibaljosh@gmail.com>
2026-03-04 17:12:51 +0800

    add duplicate_handler.py to handle duplicates

---
+++ duplicate_handler.py
@@ -0,0 +1,24 @@
+import os
+import hashlib
+
+
+class DuplicateHandler:
+    def __init__(self):
+        self.hash_store = {}
+        self.duplicate_count = 0
+        
+    def generate_hash(self, file, algorithm: str = 'sha256') -> str:
+        with open(file, 'rb') as f:
+            digest = hashlib.file_digest(f, algorithm)
+        return digest.hexdigest()
+    
+    def is_duplicate(self, file_path):
+        file_hash = self.generate_hash(file_path)
+        
+        if file_hash in self.hash_store:
+            self.duplicate_count += 1
+            return True
+        
+        self.hash_store[file_hash] = file_path
+        return False
+    
