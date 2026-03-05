ab55c16db36417d9c0b3d33fda0ac047589f3e2c
Josh112406 <quibaljosh@gmail.com>
2026-03-05 10:27:55 +0800

    add CLI interface using argparse

--- file_organizer.py
+++ file_organizer.py
@@ -1,2 +1,19 @@
 import argparse
+from organizer.organizer import FileOrganizer
+from organizer.duplicate_handler import DuplicateHandler
 
+def main():
+    parser = argparse.ArgumentParser(
+        description="Organize files in folder by category and detect duplicates."
+    )
+    
+    parser.add_argument("path", help="Path to the folder you want to organize.")
+    args = parser.parse_args()
+    duplicate_handler = DuplicateHandler
+    organizer = FileOrganizer(args.path, duplicate_handler)
+    
+    organizer.organize()
+    organizer.print_summary()
+
+if __name__ == "__main___":
+    main()
